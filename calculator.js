3029755f6ebf9802345caaf05e39f45b21fad052
Seyed Ruhallah Qasemi <70513884+Ruhallah1991@users.noreply.github.com>
2023-09-15 13:11:53 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,12 +1,18 @@
 function calculate() {
-  var R = document.getElementById("resistance").value;
-  var C = document.getElementById("capacitance").value;
-  
-  if(R && C) {
-    var tau = R * C;
-    document.getElementById("result").innerHTML = "Time Constant: " + tau + " seconds";
+  var R = parseFloat(document.getElementById("resistance").value);
+  var C = parseFloat(document.getElementById("capacitance").value);
+  var R_unit = parseFloat(document.getElementById("res_unit").value);
+  var C_unit = parseFloat(document.getElementById("cap_unit").value);
+
+  if (R && C) {
+    var tau = R * R_unit * C * C_unit;
+    var freq = 1 / (2 * Math.PI * tau);
+    
+    var result = "Time Constant (tau): " + tau + " seconds<br>";
+    result += "Filter Corner Frequency: " + freq + " Hz";
+    
+    document.getElementById("result").innerHTML = result;
   } else {
-    document.getElementById("result").innerHTML = "Please enter both Resistance and Capacitance.";
+    document.getElementById("result").innerHTML = "Please enter both Resistance and Capacitance values.";
   }
 }
-
